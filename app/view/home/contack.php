f9391116961e3f363ccbd73f271ec3cf3d5703b2
soifant <soifantoko222@gmail.com>
2022-09-18 19:04:29 +0700

    Update contack and template

--- app/view/home/contack.php
+++ app/view/home/contack.php
@@ -4,15 +4,20 @@
     	Hubungi saya jika kamu butuh sesuatu mengenai pengembangan web, saya akan sangat senang membantu.
     </div>
     <div class="bg-light mb-2 p-2 rounded">
-    <span class="badge bg-secondary">✓</span> 085777777
+    <a class="black-a text-decoration-none" href="https://wa.me/6281216128762" >
+    <span class="badge bg-secondary"><i class="fa-brands fa-whatsapp"></i></span> 085712542497
+    </a>
     </div>
     <div class="bg-light mb-2 p-2 rounded">
-    <span class="badge bg-secondary">&</span> soifant
+    <span class="badge bg-secondary"><i class="fa-solid fa-envelope"></i></span> soifantoko222@gmail.com
     </div>
     <div class="bg-light mb-2 p-2 rounded">
-    <span class="badge bg-secondary">#</span> @soifant
+   <a class="black-a text-decoration-none" href="https://twitter.com/soifant_" >
+   <span class="badge bg-secondary"><i class="fa-brands fa-twitter"></i></span> @soifant_
+    </a>
     </div>
     <div class="bg-light mb-3 p-2 rounded">
-    <span class="badge bg-secondary">@</span> @soifant
-    </div>
+    <a class="black-a text-decoration-none" href="https://github.com/soifant" >
+    <span class="badge bg-secondary"><i class="fa-brands fa-github"></i></span> soifant
+    </a></div>
     
--- app/view/template/header.php
+++ app/view/template/header.php
@@ -30,6 +30,8 @@
   color:#333;
   }
     </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
